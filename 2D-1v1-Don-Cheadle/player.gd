acc5fd66632f2a8f44e758e57ad9cf8b99a2b5a8
Sean Suggitt <s.suggitt01@gmail.com>
2025-05-19 19:30:08 -0600

    Test Level. added variable jump height and movement accel/decel

--- 2D-1v1-Don-Cheadle/player.gd
+++ 2D-1v1-Don-Cheadle/player.gd
@@ -1,34 +1,39 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var speed = 300.0
+var acceleration = 0.1
+var deceleration = 0.1
 
+const JUMP_VELOCITY = -400.0
+var decelerate_on_jump_release = 0.01
 
 func _physics_process(delta: float) -> void:
+
+	# Get the animated sprite node
+	var sprite = $AnimatedSprite2D
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+ 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= decelerate_on_jump_release
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-
-		# Get the animated sprite node
-	var sprite = $AnimatedSprite2D
+	var direction := Input.get_axis("left", "right") # returns -1(left) 1(right) 0(neither)
 
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * speed, speed * acceleration)
 		sprite.flip_h = direction < 0
 		sprite.play("run")
 	elif direction != 0:
 		sprite.play("jump")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed * deceleration)
 		sprite.play("idle")
 		
 
