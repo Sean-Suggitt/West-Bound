4c1bcddd577149219cfee7e5b8d9baaeaea2820f
Sean Suggitt <s.suggitt01@gmail.com>
2025-05-20 01:13:12 -0600

    tried adding gun stuff

--- 2D-1v1-Don-Cheadle/Scripts/player_2.gd
+++ 2D-1v1-Don-Cheadle/Scripts/player_2.gd
@@ -4,9 +4,20 @@ extends CharacterBody2D
 var speed = 300.0
 var acceleration = 0.1
 var deceleration = 0.1
-
+@onready var GunPoint = $GunPoint
 const JUMP_VELOCITY = -400.0
 var decelerate_on_jump_release = 0.01
+
+func spawnGun():
+	#preload gun scene then create an instance of it
+	var gunScene = preload("res://Scenes/gun.tscn")
+	var gunInstance = gunScene.instantiate()
+	
+	gunInstance.name = "P2Gun"
+	
+	#add the new gun instance as a child to the player node
+	GunPoint.add_child(gunInstance)
+	
 
 func _physics_process(delta: float) -> void:
 
@@ -29,12 +40,27 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = move_toward(velocity.x, direction * speed, speed * acceleration)
 		sprite.flip_h = direction < 0
+		
+		if direction == 1:
+			GunPoint.position.x = abs(GunPoint.position.x)
+		if direction == -1:
+			GunPoint.position.x = abs(GunPoint.position.x) * -1
+
+
 		sprite.play("run")
+		
 	elif direction != 0:
 		sprite.play("jump")
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed * deceleration)
 		sprite.play("idle")
 		
 
 	move_and_slide()
+
+
+
+# called when the node first enters the scene
+func _ready() -> void:
+	spawnGun()
